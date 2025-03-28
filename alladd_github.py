499f158c445ab7b63c1819e9ef2d01a83e5fcf6a
danniel_elevencione <elevencionedanniel@gmail.com>
2025-03-29 05:57:16 +0800

    Updated all files

---
+++ alladd_github.py
@@ -0,0 +1,11 @@
+print("How to Commit All Added Files in GitHub")
+
+'''
+git status  # check for changes
+
+git add .  # stage all changes
+
+git commit -m "Updated all files"  # stage all changes
+
+git push origin main   # push to GitHub:
+'''
