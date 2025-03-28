7cc7be8289a261faa1aba4a7816430461cf42ad3
danniel_elevencione <elevencionedanniel@gmail.com>
2025-03-29 05:16:53 +0800

    Updated filename.py with new changes

---
+++ python_practice1.py
@@ -0,0 +1,32 @@
+'''
+Python Practice Task: Formatted Output Challenge
+Task:
+Write a Python program that displays the following 
+formatted text using print():
+
+Name: [Your Name]  
+Age: [Your Age]  
+
+Favorite Quote:
+    "The only way to do great work  
+     is to love what you do."  
+                   - Steve Jobs
+
+'''
+#note: 
+# \n for new line
+# \t for indentation
+# \" for quotation
+# , for space
+
+#code
+name = "Danniel"
+age = 21
+
+print("Name:", name)
+print("Age;", age)
+
+print("\nFavoite Quote:"
+"\n\t\"The only way to do great work"
+"\n\t is to love what you do.\""
+"\n\t\t\t- Steve Jobs")
