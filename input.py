bc26ae03d9f5ae50c1fc81a11aabf53510546cfa
danniel_elevencione <elevencionedanniel@gmail.com>
2025-03-29 06:28:23 +0800

    Updated all files

---
+++ input.py
@@ -0,0 +1,32 @@
+print("General Format for Input in Python")
+'''
+variable = input("Prompt message: ")
+'''
+
+print("\nBasic Input Example")
+name = input("Enter your name: ")
+print("Hello,", name)
+
+'''
+Explanation:
+
+input("Enter your name: ") → Asks the user for input.
+
+The entered value is stored in the variable name.
+
+print("Hello,", name) → Prints the value entered by the user.
+'''
+
+print("\nInteger Input")
+age = int(input("Enter your age: "))
+print("Your age is:", age)
+
+print("\nFloat Input")
+decimal = float(input("Enter a decimal number: "))
+print("Decimal:, decimal")
+
+print("\nBasic Boolean Input")
+user_input = input("Enter True or False: ")  
+bool_value = user_input == "True"  
+print(bool_value)  
+
