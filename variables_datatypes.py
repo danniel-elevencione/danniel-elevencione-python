834a79ffe4f24704e0bd83e94a914876348042cf
danniel_elevencione <elevencionedanniel@gmail.com>
2025-03-29 03:48:40 +0800

    Added variables_datatypes.py

---
+++ variables_datatypes.py
@@ -0,0 +1,24 @@
+print("Variables & Data Types\n")
+
+name = "Danniel"   # String
+age = 22        # Integer
+height = 5.9    # Float
+is_student = True  # Boolean
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+''' 
+Data Types:
+
+str → Text ("Hello")
+
+int → Whole numbers (10)
+
+float → Decimal numbers (3.14)
+
+bool → True or False (True)
+
+'''
