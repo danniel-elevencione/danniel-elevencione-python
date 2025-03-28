6ea7d29b46988bd50c10e5c09262492f512bf5d1
danniel_elevencione <elevencionedanniel@gmail.com>
2025-03-29 05:49:07 +0800

    Updated all files

---
+++ removefile_github.py
@@ -0,0 +1,12 @@
+print("How to Remove a File from GitHub")
+
+'''
+git status   # check deleted files
+
+git add -A   # stage
+
+git commit -m "Removed [filename]"   # commit the change  
+
+git push origin main   # push to github
+
+'''
---
+++ updatefile_github.py
@@ -0,0 +1,12 @@
+print("How to Update a Changed File in GitHub")
+
+'''
+git status  # check for changes
+
+git add [filename]  # stage the updated file
+
+git commit -m "Updated [filename]"  # commit the changes
+
+git commit -m "Updated [filename]"  # push to GitHub
+
+'''
