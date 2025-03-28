d8c0bf34be6eb3b71f346932a5cc8b69b282c76f
danniel_elevencione <elevencionedanniel@gmail.com>
2025-03-29 02:46:21 +0800

    Added new file: comments_in_code.py

---
+++ comments_in_code.py
@@ -0,0 +1,17 @@
+print("Comments in code")
+
+# This is a single-line comment
+
+''' This is a multi-line comment
+This is a multi-line comment
+This is a multi-line comment '''
+
+""" This is also a multi-line comment
+This is also a multi-line comment
+This is also a multi-line comment """
+
+"""
+Tip: Multi-line comments using 
+triple quotes are also used as docstrings 
+(documentation for functions and classes).
+"""
