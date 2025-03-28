e786de326b87d464d75e10159aa5bf995cc69e55
danniel_elevencione <elevencionedanniel@gmail.com>
2025-03-29 03:13:24 +0800

    Added newfile_github.py

---
+++ newfile_github.py
@@ -0,0 +1,12 @@
+print("How to Add a New File to GitHub Repository")
+
+'''
+git status # Check the status of the repository
+           # "Untracked" (not yet added to Git).
+
+git add newfile.py   # Add file to staging area
+
+git commit -m "Added newfile.py"  # Commit changes 
+
+git push origin main  # Push to GitHub
+'''  
