712bb152ea22f280caf764e308c4b2d419ddcbc4
danniel_elevencione <elevencionedanniel@gmail.com>
2025-03-30 02:25:45 +0800

    Updated all files.

---
+++ arithmetic_operators.py
@@ -0,0 +1,58 @@
+# ============================================================ #
+
+# Arithmetic Operators
+
+# ============================================================ #
+
+print("\n#", "="*60, "#\n", 
+
+      " "*20, "Arithmetic Operators",
+
+      "\n#", "="*60, "#\n"
+      )
+
+operators = r'''- Arithmetic Operators -
+a + b  # Addition
+a - b  # Subtraction
+a * b  # Multiplication
+a / b  # Division (float)
+a // b  # Floor Division (integer result)
+a % b  # Modulus (remainder)
+a ** b  # Exponentiation (power)   Note: right-sided binding
+
+# Root Operations
+a ** 0.5  # Square Root
+a ** (1/3)  # Cube Root
+a ** (1/n)  # nth Root (general formula)
+
+import math
+math.sqrt(a)  # Square Root using math module
+math.pow(a, 1/n)  # nth Root using math module
+
+##################################################################
+
+Remember: 
+It's possible to formulate the following rules based 
+on this result:
+
+- when both ** arguments are integers, 
+the result is an integer, too;
+- when at least one ** argument is a float, 
+the result is a float, too.
+# also applicable for multplication, division, floor and modulo
+
+##################################################################
+
+- List of priorities -
+Priority   #Operator
+1          # **            (Highest priority)
+2          # +, -
+3          # *, /, //, %
+4          # +, -          (Lowest priority)
+
+'''
+
+print(operators)
+
+
+print("\n---end", "\n#", "="*60, "#")
