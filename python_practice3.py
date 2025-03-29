a2d3e1d05f2d98834a5e876316e94eb2c754799d
danniel_elevencione <elevencionedanniel@gmail.com>
2025-03-30 00:04:21 +0800

    Added python_practice3.py

---
+++ python_practice3.py
@@ -0,0 +1,33 @@
+# ============================================================ #
+
+# Python literals - strings
+
+# ============================================================ #
+
+print("#", "="*60, "#\n",
+    
+    "Python literals - strings",
+
+    "\n#", "="*60, "#\n",
+      
+    "Scenario:\n",
+
+    "\tWrite a one-line piece of code, \n\tusing the print() function, \n\tas well as the newline and escape characters, \n\tto match the expected result outputted on three lines.\n"
+      
+    )
+
+'''
+Expected output
+
+"I'm"
+""learning""
+"""Python"""
+
+'''
+
+# Code:
+
+print('"I\'m"', '\n""learning""', '\n"""Python"""')
+
+print("\n-end", "\n#", "="*60, "#")
+    
