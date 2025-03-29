c37871e6e841d31d6f61dd434df90d5e10c080d8
danniel_elevencione <elevencionedanniel@gmail.com>
2025-03-30 07:22:28 +0800

    Update all files.

---
+++ python_practice4.py
@@ -0,0 +1,100 @@
+#============================================================#
+
+# Variables
+
+#============================================================#
+
+print("\n#" + "="*60 + "#\n", 
+
+      " "*25, "Variables",
+
+      "\n#" + "="*60  + "#"
+      )
+
+# print("\n\n#" + "="*60  + "#\n") 
+# Printed border
+
+#============================================================#
+
+print("\nSolving simple mathematical problems")
+
+notes3 = r'''
+a = 3.0
+b = 4.0 
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+
+'''
+
+print(notes3)
+
+a = 3.0
+b = 4.0 
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
+      
+print("\n\n#" + "="*60  + "#\n")
+
+#============================================================#
+
+notes4 = r'''
+### Scenario
+#John has 3 apples, Mary has 5, and Adam has 6.  
+
+### Your Task:
+
+1. Create variables `john`, `mary`, and `adam`, assigning their 
+apple counts.  
+2. Print all three variables on one line, separated by commas.  
+3. Create `total_apples` as the sum of their apples.  
+4. Print `total_apples`.  
+5. Experiment by creating new variables, performing arithmetic 
+operations, and printing strings with numbers.
+
+#============================================================#
+
+Code:
+
+john = 3
+mary = 5 
+adam = 6
+
+print("Output:", "\nJohn =", john, "\nMary =", mary, 
+      "\nAdam =", adam)
+
+
+total_apples = john + mary + adam
+
+print("\nTotal apples =", total_apples)
+
+
+#============================================================#
+'''
+
+print(notes4)
+
+john = 3
+mary = 5 
+adam = 6
+
+print("Output:", "\nJohn =", john, "\nMary =", mary, 
+      "\nAdam =", adam)
+
+
+total_apples = john + mary + adam
+
+print("\nTotal apples =", total_apples, "\n")
+
+
+#============================================================#
+
+print("\n#" + "="*60 + "#\n", 
+
+      " "*20, "- - - End - - -",
+
+      "\n#" + "="*60  + "#"
+      )
+
+### End
+
+#============================================================#
