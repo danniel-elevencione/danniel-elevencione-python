1629ff5c471a8c02659aa7cfd5fc57d65d099591
danniel_elevencione <elevencionedanniel@gmail.com>
2025-04-01 03:13:47 +0800

    Added lesson_outline.py.

---
+++ Python General/lesson_outline.py
@@ -0,0 +1,198 @@
+#============================================================#
+
+# Python Essentials 1 - Lesson Outline by CISCO
+
+#============================================================#
+
+### Header ###
+
+print("\n#" + "="*60 + "#\n", 
+
+      " "*7, "Python Essentials 1 - Lesson Outline by CISCO",
+
+      "\n#" + "="*60  + "#"
+      )
+
+# print("\n\n#" + "="*60  + "#\n") 
+# Printed border
+
+#============================================================#
+
+### Body Start ###
+
+notes = r'''
+### PE1: Module 1 ### 
+
+Introduction to Python and Computer Programming
+
+#------------------------------------------------------------#
+
+### 1.0. Welcome to Python Essentials 1
+
+1.0.1 Learn Python – the language of today and tomorrow
+1.0.2 About the course
+1.0.3 Syllabus
+1.0.4 Prepare for the PCEP-30-0x exam
+
+#------------------------------------------------------------#
+
+### 1.1. Section 1 – Introduction to Programming
+
+1.1.1 How does a computer program work?
+1.1.2 Natural languages vs. programming languages
+1.1.3 What makes a language?
+1.1.4 Machine language vs. high-level language
+1.1.5 Compilation vs. interpretation
+1.1.6 What does the interpreter do?
+1.1.7 Compilation vs. interpretation – advantages 
+      and disadvantages
+
+#------------------------------------------------------------#    
+
+### 1.2. Section 2 – Introduction to Python
+
+1.2.1 Python – a tool, not a reptile
+1.2.2 Who created Python?
+1.2.3 A hobby programming project
+1.2.4 What makes Python so special?
+1.2.5 Python rivals?
+1.2.6 Where can we see Python in action?
+1.2.7 Why not Python?
+1.2.8 There is more than one Python
+1.2.9 Python implementations
+
+#------------------------------------------------------------#
+
+### 1.3. Section 3 – Downloading and Installing Python
+
+1.3.1 Begin your Python journey
+1.3.2 How to download, install, and configure Python
+1.3.3 Starting your work with Python
+1.3.4 Your very first program before your first program...
+1.3.5 How to spoil and fix your code
+
+#------------------------------------------------------------#
+
+### 1.4. Module 1 Completion – Module Test
+
+
+#============================================================#
+
+### PE1: Module 2 ###
+
+Python Data Types, Variables, Operators, 
+and Basic I/O Operations
+
+#------------------------------------------------------------#
+
+### 2.1. Section 1 – The "Hello, World!" Program
+
+2.1.1 Your very first program
+2.1.2 The print() function
+2.1.3 Function arguments
+2.1.4 Function invocation
+2.1.5 LAB Working with the print() function
+2.1.6 The print() function and its effect, arguments, 
+      and values returned
+2.1.7 Instructions
+2.1.8 Python escape and newline characters
+2.1.9 Using multiple arguments
+2.1.10 Positional arguments
+2.1.11 Keyword arguments
+2.1.12 LAB The print() function and its arguments
+2.1.13 LAB Formatting the output
+2.1.14 SECTION SUMMARY
+2.1.15 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+### 2.2. Section 2 – Python literals
+
+2.2.1 Literals – the data in itself
+2.2.2 Integers
+2.2.3 Floats
+2.2.4 Strings
+2.2.5 Boolean values
+2.2.6 LAB Python literals - strings
+2.2.7 SECTION SUMMARY
+2.2.8 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+### 2.3. Section 3 – Operators - data manipulation tools
+
+2.3.1 Python as a calculator
+2.3.2 Basic operators
+2.3.3 Operators and their priorities
+2.3.4 SECTION SUMMARY
+2.3.5 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+### 2.4. Section 4 – Variables
+
+2.4.1 Variables – data-shaped boxes
+2.4.2 Variable names
+2.4.3 How to create a variable
+2.4.4 How to use a variable
+2.4.5 How to assign a new value to an already existing variable
+2.4.6 Solving simple mathematical problems
+2.4.7 LAB Variables
+2.4.8 Shortcut operators
+2.4.9 LAB Variables ‒ a simple converter
+2.4.10 LAB Operators and expressions
+2.4.11 SECTION SUMMARY
+2.4.12 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+### 2.5. Section 5 – Comments
+
+2.5.1 Comments – why, when, and how?
+2.5.2 Marking fragments of code
+2.5.3 LAB Comments
+2.5.4 SECTION SUMMARY
+2.5.5 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+### 2.6. Section 6 – Interaction with the user
+
+2.6.1 The input() function
+2.6.2 The input() function with an argument
+2.6.3 The result of the input() function
+2.6.4 The input() function – prohibited operations
+2.6.5 Type casting (type conversions)
+2.6.6 More about input() and type casting
+2.6.7 String operators
+2.6.8 Type conversions once again
+2.6.9 LAB Simple input and output
+2.6.10 LAB Operators and expressions
+2.6.11 LAB Operators and expressions – 2
+2.6.12 SECTION SUMMARY
+2.6.13 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+### 2.7. Module 2 Completion – MODULE TEST
+
+'''
+
+print(notes)
+
+### Body End ###
+
+#============================================================#
+ 
+### Footer ###
+
+print("\n#" + "="*60 + "#\n", 
+
+      " "*20, "- - - End - - -",
+
+      "\n#" + "="*60  + "#"
+      )
+
+### End of File ###
+
+#============================================================#
