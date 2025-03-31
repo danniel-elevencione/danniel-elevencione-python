562fe6ab4c5fcc4382e88005032d17dd2177d9f0
danniel_elevencione <elevencionedanniel@gmail.com>
2025-03-31 17:40:28 +0800

    Added

---
+++ type_conversion.py
@@ -0,0 +1,106 @@
+#============================================================#
+
+# Type Conversion
+
+#============================================================#
+
+### Header ###
+
+print("\n#" + "="*60 + "#\n", 
+
+      " "*20, "Type Conversion",
+
+      "\n#" + "="*60  + "#"
+      )
+
+# print("\n\n#" + "="*60  + "#\n") 
+# Printed border
+
+
+#============================================================#
+
+### Body Start ###
+
+notes1 = r'''
+## Type Conversion - String to Integer (`int()`)
+# The `int()` function converts a valid numeric string into an integer.
+
+num_str = "42"
+num_int = int(num_str)  
+print(num_int)  # 42 (Integer)
+
+Output:
+'''
+
+print(notes1)
+
+num_str = "42"
+num_int = int(num_str)  
+print(num_int)  # 42 (Integer)
+
+print("\n\n#" + "="*60 + "#\n")  
+# Printed border
+
+
+#============================================================#
+
+notes2 = r'''
+## Type Conversion - String to Float (`float()`)
+# The `float()` function converts a valid numeric string into a floating-point number.
+
+num_str = "3.14"
+num_float = float(num_str)
+print(num_float)  # 3.14 (Float)
+
+Output:
+'''
+
+print(notes2)
+
+num_str = "3.14"
+num_float = float(num_str)
+print(num_float)  # 3.14 (Float)
+
+print("\n\n#" + "="*60 + "#\n")  
+# Printed border
+
+
+#============================================================#
+
+notes3 = r'''
+## Type Conversion - Number to String (`str()`)
+# The `str()` function converts numbers into strings.
+
+num = 100
+num_str = str(num)
+print(num_str)  # "100" (String)
+
+Output:
+'''
+
+print(notes3)
+
+num = 100
+num_str = str(num)
+print(num_str)  # "100" (String)
+
+print("\n\n#" + "="*60 + "#\n")  
+# Printed border
+
+
+### Body End ###
+
+#============================================================#
+ 
+ ### Footer ###
+
+print("\n#" + "="*60 + "#\n", 
+
+      " "*20, "- - - End - - -",
+
+      "\n#" + "="*60  + "#"
+      )
+
+### End of File ###
+
+#============================================================#
