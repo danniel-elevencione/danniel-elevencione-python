6b70452b4f34ac0e4bd4035fc86c034255f4e104
danniel_elevencione <elevencionedanniel@gmail.com>
2025-04-01 17:52:35 +0800

    Updated lesson_outline.py

--- Python General/lesson_outline.py
+++ Python General/lesson_outline.py
@@ -75,7 +75,6 @@ Introduction to Python and Computer Programming
 
 ### 1.4. Module 1 Completion – Module Test
 
-
 #============================================================#
 
 ### PE1: Module 2 ###
@@ -176,6 +175,230 @@ and Basic I/O Operations
 
 ### 2.7. Module 2 Completion – MODULE TEST
 
+#============================================================#
+
+### PE1: Module 3 ### 
+
+Boolean Values, Conditional Execution, Loops, 
+Lists and List Processing, Logical and Bitwise Operations
+
+#------------------------------------------------------------#
+
+### 3.1. Section 1 – Making decisions in Python
+
+3.1.1 Questions and answers
+3.1.2 Comparison: equality operator
+3.1.3 Exercises
+3.1.4 Operators
+3.1.5 Making use of the answers
+3.1.6 LAB Variables ‒ Questions and answers
+3.1.7 Conditions and conditional execution
+3.1.8 Analyzing code samples
+3.1.9 Pseudocode and introduction to loops
+3.1.10 LAB Comparison operators and conditional execution
+3.1.11 LAB Essentials of the if-else statement
+3.1.12 LAB Essentials of the if-elif-else statement
+3.1.13 SECTION SUMMARY
+3.1.14 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+### 3.2. Section 2 – Loops in Python
+
+3.2.1 Looping your code with while
+3.2.2 An infinite loop
+3.2.3 The while loop: more examples
+3.2.4 LAB Guess the secret number
+3.2.5 Looping your code with for
+3.2.6 More about the for loop and the range() function with 
+      three arguments
+3.2.7 LAB Essentials of the for loop – counting mississippily
+3.2.8 The break and continue statements
+3.2.9 LAB The break statement – Stuck in a loop
+3.2.10 LAB The continue statement – the Ugly Vowel Eater
+3.2.11 LAB The continue statement – the Pretty Vowel Eater
+3.2.12 The while loop and the else branch
+3.2.13 The for loop and the else branch
+3.2.14 LAB Essentials of the while loop
+3.2.15 LAB Collatz's hypothesis
+3.2.16 SECTION SUMMARY
+3.2.17 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+### 3.3. Section 3 – Logic and bit operations in Python
+
+3.3.1 Computer logic
+3.3.2 Logical expressions
+3.3.3 Logical values vs. single bits
+3.3.4 Bitwise operators
+3.3.5 How do we deal with single bits?
+3.3.6 Binary left shift and binary right shift
+3.3.7 SECTION SUMMARY
+3.3.8 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+### 3.4. Section 4 – Lists
+
+3.4.1 Why do we need lists?
+3.4.2 Indexing lists
+3.4.3 Accessing list content
+3.4.4 Removing elements from a list
+3.4.5 Negative indices are legal
+3.4.6 LAB The basics of lists
+3.4.7 Functions vs. methods
+3.4.8 Adding elements to a list: append() and insert()
+3.4.9 Making use of lists
+3.4.10 Lists in action
+3.4.11 LAB The basics of lists ‒ the Beatles
+3.4.12 SECTION SUMMARY
+3.4.13 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+### 3.5. Section 5 – Sorting simple lists: the bubble sort algorithm
+
+3.5.1 The bubble sort
+3.5.2 Sorting a list
+3.5.3 The bubble sort – interactive version
+3.5.4 SECTION SUMMARY
+3.5.5 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+### 3.6. Section 6 – Operations on lists
+
+3.6.1 The inner life of lists
+3.6.2 Powerful slices
+3.6.3 Slices – negative indices
+3.6.4 The in and not in operators
+3.6.5 Lists – some simple programs
+3.6.6 LAB Operating with lists ‒ basics
+3.6.7 SECTION SUMMARY
+3.6.8 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+### 3.7. Section 7 – Lists in advanced applications
+
+3.7.1 Lists in lists
+3.7.2 Two-dimensional arrays
+3.7.3 Multidimensional nature of lists: advanced applications
+3.7.4 SECTION SUMMARY
+
+#------------------------------------------------------------#
+
+### 3.8. Module 3 Completion – Module Test
+
+#============================================================#
+
+### PE1: Module 4 ###
+Functions, Tuples, Dictionaries, Exceptions, 
+and Data Processing
+
+#------------------------------------------------------------#
+
+4.1. Section 1 – Functions
+4.1.1 Why do we need functions?
+4.1.2 Decomposition
+4.1.3 Where do the functions come from?
+4.1.4 Your first function
+4.1.5 How functions work
+4.1.6 SECTION SUMMARY
+4.1.7 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+4.2. Section 2 – How functions communicate with their environment
+4.2.1 Parameterized functions
+4.2.2 Positional parameter passing
+4.2.3 Keyword argument passing
+4.2.4 Mixing positional and keyword arguments
+4.2.5 Parametrized functions – more details
+4.2.6 SECTION SUMMARY
+4.2.7 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+4.3. Section 3 – Returning a result from a function
+4.3.1 Effects and results: the return instruction
+4.3.2 A few words about None
+4.3.3 Effects and results: lists and functions
+4.3.4 LAB A leap year: writing your own functions
+4.3.5 LAB How many days: writing and using your own functions
+4.3.6 LAB Day of the year: writing and using your own functions
+4.3.7 LAB Prime numbers ‒ how to find them
+4.3.8 LAB Converting fuel consumption
+4.3.9 SECTION SUMMARY
+4.3.10 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+4.4. Section 4 – Scopes in Python
+4.4.1 Functions and scopes
+4.4.2 Functions and scopes: the global keyword
+4.4.3 How the function interacts with its arguments
+4.4.4 SECTION SUMMARY
+4.4.5 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+4.5. Section 5 – Creating multi-parameter functions
+4.5.1 Sample functions: Evaluating the BMI
+4.5.2 Sample functions: Triangles
+4.5.3 Sample functions: Factorials
+4.5.4 Fibonacci numbers
+4.5.5 Recursion
+4.5.6 SECTION SUMMARY
+4.5.7 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+4.6. Section 6 – Tuples and dictionaries
+4.6.1 Sequence types and mutability
+4.6.2 Tuples
+4.6.3 Dictionaries
+4.6.4 Dictionary methods and functions
+4.6.5 Tuples and dictionaries can work together
+4.6.6 SECTION SUMMARY
+4.6.7 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+4.7. Section 7 – Exceptions
+4.7.1 Errors – the developer's daily bread
+4.7.2 When data is not what it should be
+4.7.3 The try-except branch
+4.7.4 The exception proves the rule
+4.7.5 How to deal with more than one exception
+4.7.6 The default exception and how to use it
+4.7.7 Some useful exceptions
+4.7.8 Why you can’t avoid testing your code
+4.7.9 When Python closes its eyes
+4.7.10 Tests, testing, and testers
+4.7.11 print debugging
+4.7.12 Some useful tips
+4.7.13 Unit testing – a higher level of coding
+4.7.14 SECTION SUMMARY
+4.7.15 SECTION QUIZ
+
+#------------------------------------------------------------#
+
+4.8. Module 4 Completion – Module Test
+
+#============================================================#
+
+### Python Essentials 1 (PE1) Course Final Exam
+
+Final Project
+Final Test
+End of Course Survey
+
+#============================================================#
+
+### PCEP – Certified Entry-Level Python Programmer 
+Certification Exam
 '''
 
 print(notes)
